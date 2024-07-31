59e2147967c59a279c0281dc30cedd3225c636d3
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-07-31 17:40:21 -0400

    Navegación entre vistas

--- who is that pokemon/Controller/ResultsViewController.swift
+++ who is that pokemon/Controller/ResultsViewController.swift
@@ -9,13 +9,27 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
+    @IBOutlet weak var pokemonImage: UIImageView!
+    @IBOutlet weak var pokemonLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var pokemonName: String = ""
+    var pokemonImageURL: String = ""
+    var finalScore: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scoreLabel.text = "Perdiste, tu puntaje fue de \(finalScore)."
+        pokemonLabel.text = "No, es un \(pokemonName)"
+        pokemonImage.kf.setImage(with: URL(string: pokemonImageURL))
 
-        // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func playAgainPressed(_ sender: UIButton) {
+        self.dismiss(animated: true)
+    }
+    
     /*
     // MARK: - Navigation
 
